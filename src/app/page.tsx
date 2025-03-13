38004640521a0102540cf7060761fcfbfdc87ac0
amaraa-11 <amaraamygmarjaw@gmail.com>
2025-03-13 17:38:38 +0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Template from "./_components/template";
+
 export default function Home() {
   return (
-  <div> <Template> Home page</Template></div>
+    <div>
+      <Template>Home</Template>
+    </div>
   );
 }
