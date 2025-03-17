768bafd04b894ee9b4181e946c75feee5678bfac
24LP8971 24LP8971 <24LP8971@Setups-iMac.local>
2025-03-17 15:04:44 +0800

    or

--- src/app/_components/categoryComp.tsx
+++ src/app/_components/categoryComp.tsx
@@ -1,5 +1,6 @@
 import HomeHeader from "./homeHeader";
 import Footer from "./homeFooter";
+import Image from "next/image";
 import CategoryFilterSide from "./categoryFilterSide";
 export default function categoryComp() {
   return (
@@ -7,7 +8,15 @@ export default function categoryComp() {
       <HomeHeader />
       <div className="flex">
         <CategoryFilterSide />
-        <div className="w-[80%] h-[1200px] bg-gray-600">test 12</div>
+        <div className="w-[70%] h-[1200px] bg-white rounded-3xl border">
+          <div className="w-[280px] h-[360px] m-6  border shadow-sm  ">
+            <Image src={"/t-shirt.png"} width={280} height={280} alt="item" />
+            <div className="bg-blue-400 p-3 ">
+              <p className="font-bold text-xl">Tolbotoi podwolk</p>
+              <p className="font-medium  text-md">120$</p>
+            </div>
+          </div>
+        </div>
       </div>
       <Footer />
     </div>
