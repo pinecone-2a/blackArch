6d74bb884469bc5a3df2c2be8fb653a195142b3c
amaraa <amaraamygmarjaw@gmail.com>
2025-03-21 13:28:43 +0800

    27 payment frontend (#48)
    * 12 admin dashboard (#26)
    
    * validate on auth
    
    * forgot pass
    
    * fix
    
    * admin products
    
    * fix
    
    * fixed by nandir bagsh
    
    * fixed minor
    
    * maybe fixed
    
    * 12
    
    * amaraa
    
    * update payment
    
    * commit
    
    * deploy
    
    * 23
    
    * commit
    
    * commit
    
    ---------
    
    Co-authored-by: Boldchingis <bboldchingis@gmail.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
-import { ReactLenis } from "./utils/lenis"
+import ReactLenis from "lenis/react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -32,11 +32,11 @@ export default function RootLayout({
         <link rel="icon" type="image/png" href="/shopping-cart.png" />
       </Head>
       <ReactLenis root>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {children}
+        </body>
       </ReactLenis>
     </html>
   );
