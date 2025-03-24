efedd85583b88ccfc2b3906d3b37f8e0f231d5a5
amaraa <amaraamygmarjaw@gmail.com>
2025-03-24 14:18:09 +0800

    27 payment frontend (#59)
    * 12 admin dashboard (#26)
    
    * validate on auth
    
    * forgot pass
    
    * fix
    
    * admin products
    
    * fix
    
    * fixed by nandir bagsh
    
    * fixed minor
    
    * maybe fixed
    
    * 12
    
    * amaraa
    
    * update payment
    
    * commit
    
    * deploy
    
    * 23
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit amka
    
    * update
    
    * update
    
    * commit
    
    * commit
    
    ---------
    
    Co-authored-by: Boldchingis <bboldchingis@gmail.com>

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,20 +1,26 @@
 import prisma from "@/lib/connect";
- import { NextResponse } from "next/server";
- 
- export const GET = async (req: Request, { params }: { params: { id: string } }) => {
-     try {
-       const { id } = params;
-       const product = await prisma.product.findUnique({
-         where: { id: id },
-       });
-   
-       if (!product) {
-         return NextResponse.json({ error: "Product not found", status: 404 });
-       }
-   
-       return NextResponse.json({ message: product, status: 200 });
-     } catch (error) {
-       console.error("Error fetching product:", error);
-       return NextResponse.json({ error: "Internal Server Error", status: 500 });
-     }
-   };
+import { NextResponse } from "next/server";
+
+export const GET = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { id } = params;
+    const product = await prisma.product.findUnique({
+      where: { id: id },
+    });
+
+    if (!product) {
+      return NextResponse.json({ error: "Product not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(product, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching product:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+};
