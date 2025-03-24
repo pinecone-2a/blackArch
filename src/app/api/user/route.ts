22207c6b51d38c7af1031f3d0be8b1b89ca3ced8
amaraa-11 <amaraamygmarjaw@gmail.com>
2025-03-24 17:59:03 +0800

    update

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,15 +1,15 @@
 import { getUserFromToken } from "@/lib/auth/getUser";
 import { NextRequest, NextResponse } from "next/server";
 
-export const GET = async (req: NextRequest, res: NextResponse) => {
+export async function GET(req: NextRequest, _context: { params: {} }) {
   const user = getUserFromToken(req);
 
   if (!user) {
-    return NextResponse.json({ error: "Unauthorized Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Unauthorized Server Error" },
+      { status: 500 }
+    );
   }
 
   return NextResponse.json({ user }, { status: 200 });
-};
-
-
-
+}
