53a8f4d1129d9e29433f36ab7af2b74fcf2dbb08
amaraa-11 <amaraamygmarjaw@gmail.com>
2025-03-25 13:35:52 +0800

    99

--- src/app/_components/homeHeader.tsx
+++ src/app/_components/homeHeader.tsx
@@ -1,24 +1,27 @@
-"use client"
+"use client";
 import Link from "next/link";
 import { ShoppingCart } from "lucide-react";
 import { Menu } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Search } from "lucide-react";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion";
 import Autocomplete from "@/components/algolia/AutoComplete";
 import NextInstantSearch from "@/components/algolia/NextInstantSearch";
 import { searchClient } from "@/lib/algolia/searchClient";
 import { INSTANT_SEARCH_INDEX_NAME } from "@/lib/constants/types";
-import { Configure, } from "react-instantsearch";
+import { Configure } from "react-instantsearch";
 import { Hits } from "react-instantsearch";
 import SearchAlgolia from "@/components/algolia/Search";
 
 export default function Navbar() {
   return (
-    <div className='sticky  w-full mx-auto flex px-10 top-0 bg-white z-20 pb-4'>
+    <div className="sticky  w-full mx-auto flex px-10 top-0 bg-white z-20 pb-4">
       <nav className="bg-white w-full p-4 xsm:px-6 md:px-24 pb-0 py-4 flex items-center gap-12 justify-between">
         <span>
-          <Link href={"/"} className="text-2xl sm:text-xl md:text-2xl lg:text-3xl font-extrabold">
+          <Link
+            href={"/"}
+            className="text-2xl sm:text-xl md:text-2xl lg:text-3xl font-extrabold"
+          >
             PINESHOP
           </Link>
         </span>
@@ -40,9 +43,6 @@ export default function Navbar() {
           </div>
         </div>
 
-  
-
-
         <div className="flex items-center gap-3 min-w-[150px] border-b border-gray-500 pb-2">
           <motion.input
             type="text"
@@ -57,20 +57,16 @@ export default function Navbar() {
           >
             <Search className="w-5 h-5 text-gray-700" />
           </motion.button>
-
         </div>
-    
 
         <div className="flex gap-3 xsm:mt-3">
-          <div className='relative'>
+          <div className="relative">
             <Link href={"/cart"}>
-
               <ShoppingCart />
-
             </Link>
           </div>
           <div className="hidden">
-            <Menu/>
+            <Menu />
           </div>
         </div>
       </nav>
--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -1,29 +1,23 @@
 import prisma from "@/lib/connect";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-
-
-export const GET = async (req: NextRequest,) => {
-
-    const id = req.nextUrl.searchParams.get("id");
-
+export const GET = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
   try {
-
-    if (!id) {
-      return NextResponse.json({ error: "Missing product ID" }, { status: 400 });
-    }
-
+    const { id } = params;
     const product = await prisma.product.findUnique({
-      where: { id },
+      where: { id: id },
     });
 
     if (!product) {
-      return NextResponse.json({ error: "Product not found" }, { status: 404 });
+      return NextResponse.json({ error: "Product not found", status: 404 });
     }
 
-    return NextResponse.json(product, { status: 200 });
+    return NextResponse.json({ message: product, status: 200 });
   } catch (error) {
     console.error("Error fetching product:", error);
-    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json({ error: "Internal Server Error", status: 500 });
   }
 };
