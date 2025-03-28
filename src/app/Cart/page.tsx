5653a0d7d5c184461ac9c835216aa6df98acc890
Ozuuneee <ozunegaming@gmail.com>
2025-03-28 12:53:40 +0800

    9 cart frontend (#47)
    * commit
    
    * cart
    
    * big change
    
    * changes
    
    * 27 payment frontend (#65) (#66)
    
    * 27 payment frontend (#65)
    
    * 12 admin dashboard (#26)
    
    * validate on auth
    
    * forgot pass
    
    * fix
    
    * admin products
    
    * fix
    
    * fixed by nandir bagsh
    
    * fixed minor
    
    * maybe fixed
    
    * 12
    
    * amaraa
    
    * update payment
    
    * commit
    
    * deploy
    
    * 23
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit amka
    
    * update
    
    * update
    
    * commit
    
    * commit
    
    * commit
    
    * commit2
    
    ---------
    
    Co-authored-by: Boldchingis <bboldchingis@gmail.com>
    
    * update
    
    * 27 payment frontend (#67)
    
    * 12 admin dashboard (#26)
    
    * validate on auth
    
    * forgot pass
    
    * fix
    
    * admin products
    
    * fix
    
    * fixed by nandir bagsh
    
    * fixed minor
    
    * maybe fixed
    
    * 12
    
    * amaraa
    
    * update payment
    
    * commit
    
    * deploy
    
    * 23
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit amka
    
    * update
    
    * update
    
    * commit
    
    * commit
    
    * commit
    
    * commit2
    
    * commit
    
    ---------
    
    Co-authored-by: Boldchingis <bboldchingis@gmail.com>
    
    ---------
    
    Co-authored-by: amaraa <amaraamygmarjaw@gmail.com>
    Co-authored-by: Boldchingis <bboldchingis@gmail.com>
    
    ---------
    
    Co-authored-by: 24LP6831 24LP6831 <24LP6831@setups-iMac-2.local>
    Co-authored-by: Chinguun <centillionc0919@gmail.com>
    Co-authored-by: amaraa <amaraamygmarjaw@gmail.com>
    Co-authored-by: Boldchingis <bboldchingis@gmail.com>

---
+++ src/app/Cart/page.tsx
@@ -0,0 +1,11 @@
+import Cart from "../_components/cart";
+import Footer from "../_components/Footer";
+
+export default function CartPage() {
+  return (
+    <div>
+      <Cart />
+      <Footer />
+    </div>
+  );
+}
