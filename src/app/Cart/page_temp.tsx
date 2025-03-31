5cdd30cc4abae3e2f7e25fdfa2bf6dbae648d1f4
REINNn0820 <nymaa4764@gmail.com>
2025-03-31 16:07:39 +0800

    j

--- src/app/Cart/page_temp.tsx
+++ src/app/Cart/page_temp.tsx
@@ -1,10 +1,6 @@
 import Cart from "../_components/cart";
-<<<<<<< HEAD:src/app/cart/page.tsx
-import Footer from "../_components/Footer";
-=======
 import Footer from "../_components/homeFooter";
 import Navbar from "../_components/homeHeader";
->>>>>>> 4a8bda436e3a14f990c2f8900f4d88277f9db736:src/app/Cart/page_temp.tsx
 
 export default function CartPage() {
   return (
