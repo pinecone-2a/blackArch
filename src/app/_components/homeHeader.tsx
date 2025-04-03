8a383d5d77ad996fac9c13c977070401b7cdc730
Amaraa <amaraamygmarjaw@gmail.com>
2025-04-03 15:49:24 +0800

    27 payment frontend (#85)
    * 12 admin dashboard (#26)
    
    * validate on auth
    
    * forgot pass
    
    * fix
    
    * admin products
    
    * fix
    
    * fixed by nandir bagsh
    
    * fixed minor
    
    * maybe fixed
    
    * 12
    
    * amaraa
    
    * update payment
    
    * commit
    
    * deploy
    
    * 23
    
    * commit
    
    * commit
    
    * commit
    
    * commit
    
    * commit amka
    
    * update
    
    * update
    
    * commit
    
    * commit
    
    * commit
    
    * commit2
    
    * commit
    
    * m
    
    * k
    
    * a
    
    * amka
    
    * g
    
    ---------
    
    Co-authored-by: Boldchingis <bboldchingis@gmail.com>

--- src/app/_components/homeHeader.tsx
+++ src/app/_components/homeHeader.tsx
@@ -96,8 +96,6 @@ export default function Navbar() {
               </Link>
               <span className="absolute bottom-0 left-0 w-full h-[2px] bg-[#d94f5c] scale-x-0 transition-all duration-300 group-hover:scale-x-100"></span>
             </p>
-
-
           </div>
         </div>
 
