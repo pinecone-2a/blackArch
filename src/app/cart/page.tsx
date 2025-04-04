b8930f6138153f6112ad63aa0c3f71cd682abfa2
24LP6831 24LP6831 <24LP6831@setups-iMac-2.local>
2025-04-04 12:57:11 +0800

    fi

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Cart from "../_components/cart";
+import Footer from "../_components/homeFooter";
+import Navbar from "../_components/homeHeader";
+import Template from "../_components/template";
+
+export default function CartPage() {
+  return (
+    <div>
+      <Template>
+        <Navbar />
+        <Cart />
+        <Footer />
+      </Template>
+    </div>
+  );
+}
